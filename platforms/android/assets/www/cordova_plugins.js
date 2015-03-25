3cee0591919fbc408eff1014484c25e20f410a21
jointheflow <gianluca.ragnoni@gmail.com>
2015-03-25 17:57:52 +0100

    Managing splash screen with the plugin, omptimizing images

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -13,13 +13,21 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.Focus"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.ionic.keyboard": "1.0.4",
-    "fr._46cl.focus": "0.1.3"
+    "fr._46cl.focus": "0.1.3",
+    "org.apache.cordova.splashscreen": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
